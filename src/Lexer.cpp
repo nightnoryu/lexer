aeb2a659945be8db22e622421ebce131e9a65b16
m3tro1d <qazwf734@gmail.com>
2023-01-08 13:22:38 +0300

    Implement string literal parsing

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -24,37 +24,90 @@ std::vector<Token> Lexer::Parse(std::istream& input)
 
 			switch (currentValue)
 			{
+			case ' ':
+				token.type = TokenType::BLANK;
+				break;
+			case '#':
+				token.type = TokenType::COMMENT;
+				token.lexeme = "#";
+				break;
+
+			case '\'':
+				token.type = TokenType::STRING_LITERAL;
+				token.lexeme = ParseStringLiteral(line, i);
+				break;
+
 			case '+':
 				token.type = TokenType::ADDITION;
 				token.lexeme = "+";
-				++i;
 				break;
 			case '-':
-				token.type = TokenType::SUBTRACTION,
-				token.lexeme = "-",
-				++i;
+				token.type = TokenType::SUBTRACTION;
+				token.lexeme = "-";
 				break;
 			case '*':
-				token.type = TokenType::MULTIPLICATION,
-				token.lexeme = "*",
-				++i;
+				token.type = TokenType::MULTIPLICATION;
+				token.lexeme = "*";
 				break;
 			case '/':
-				token.type = TokenType::DIVISION,
-				token.lexeme = "+",
-				++i;
+				token.type = TokenType::DIVISION;
+				token.lexeme = "+";
 				break;
+
+			case '=':
+				if (nextValue == '=')
+				{
+					token.type = TokenType::EQUAL;
+					token.lexeme = "==";
+					++i;
+				}
+				else
+				{
+					token.type = TokenType::ASSIGNMENT;
+					token.lexeme = "=";
+				}
+				break;
+
 			default:
 				token.type = TokenType::ERROR;
-				++i;
 				break;
 			}
 
+			if (token.type == TokenType::COMMENT)
+			{
+				i = line.length();
+			}
+
+			++i;
 			result.emplace_back(std::move(token));
 		}
 
 		++lineNumber;
 	}
+
+	return result;
+}
+
+std::string Lexer::ParseStringLiteral(std::string const& line, std::size_t& i)
+{
+	std::string result = "'";
+	auto ch = line.at(++i);
+
+	while (ch != '\'')
+	{
+		result += ch;
+
+		++i;
+		if (i == line.length())
+		{
+			break;
+		}
+
+		ch = line.at(i);
+	}
+
+	result += '\'';
+	++i;
 
 	return result;
 }
--- src/Lexer.h
+++ src/Lexer.h
@@ -8,4 +8,9 @@ class Lexer
 {
 public:
 	static std::vector<Token> Parse(std::istream& input);
+
+private:
+	static std::string ParseStringLiteral(std::string const& line, std::size_t& i);
+	static Token ParseNumber(std::string const& line, std::size_t& i);
+	static Token ParseIdentifier(std::string const& line, std::size_t& i);
 };
