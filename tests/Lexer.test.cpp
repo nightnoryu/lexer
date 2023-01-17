5b1fa548f11ecf0923f246e9161d88f7c211203a
m3tro1d <qazwf734@gmail.com>
2023-01-17 09:02:58 +0300

    Implement tests

--- tests/Lexer.test.cpp
+++ tests/Lexer.test.cpp
@@ -1,6 +1,29 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
 
-TEST_CASE("whatever")
+#include "../src/Lexer.h"
+#include "../src/TokensPrinter.h"
+#include "catch.hpp"
+#include <sstream>
+
+TEST_CASE("end2end cases")
 {
+	std::string const testFile = GENERATE("valid", "numbers");
+
+	std::ifstream input("files/" + testFile + ".txt");
+	std::stringstream output;
+
+	std::ifstream expectedOutputFile("files/output/" + testFile + ".txt");
+	std::stringstream expectedOutput;
+	expectedOutput << expectedOutputFile.rdbuf();
+
+	WHEN("parsing a file")
+	{
+		auto const tokens = Lexer::Parse(input);
+		TokensPrinter::Print(output, tokens);
+
+		THEN("result matches")
+		{
+			REQUIRE(output.str() == expectedOutput.str());
+		}
+	}
 }
